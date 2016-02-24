a7aec02ad47e288c7b2b9a5eec01d1527d37b948
havk64 <havk.fs@gmail.com>
2016-02-24 14:22:03 -0800

    Task 2 solved

---
+++ read_logs_while_you_wait/2-elsif_wants_to_join_the_party.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+HEAD=0;
+GET=0;
+while read line
+do
+	if echo $line | grep -q "HEAD"
+	then
+		((HEAD++)) 
+	elif echo $line | grep -q "GET"
+	then
+		((GET++))
+	fi
+done < $1;
+echo $HEAD;
+echo $GET;
+#printf("%d\n", $HEAD);
+#printf("%d\n", $GET);
