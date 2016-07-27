4132f3d96142f6bf4b42ea6a5519fab811265e2b
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-07-27 17:26:15 -0300

    Added holberton-wrapper cookbook

---
+++ chef_is_fun/cookbooks/holberton-wrapper/metadata.rb
@@ -0,0 +1,7 @@
+name             'holberton-wrapper'
+maintainer       'Alexandro de Oliveira'
+maintainer_email 'alexandro.oliveira@holbertonschool.com'
+license          'All rights reserved'
+description      'Installs/Configures holberton-wrapper'
+long_description 'Installs/Configures holberton-wrapper'
+version          '0.1.0'
---
+++ chef_is_fun/cookbooks/holberton-wrapper/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: holberton-wrapper
+# Recipe:: default
+#
+# Copyright (C) 2016 Alexandro de Oliveira
+#
+# All rights reserved - Do Not Redistribute
+#
