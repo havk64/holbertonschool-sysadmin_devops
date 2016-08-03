f1e2fdf37f76091b155a2304a1c6bfd73dce4cbd
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-08-03 04:36:33 -0300

    Task 0 completed

--- infrastructure_as_code_with_aws_ec2/0-manage_ec2_instances.rb
+++ infrastructure_as_code_with_aws_ec2/0-manage_ec2_instances.rb
@@ -1,27 +1,25 @@
 #!/usr/bin/env ruby
+
 require 'optparse'
 require 'aws-sdk'
 require 'yaml'
+require 'pp'
 
 
 Options = Struct.new(:action,:instance_id, :verbose)
-
 args = Options.new()
 
 parser = OptionParser.new do |opts|
   opts.banner = "Usage: aws_script.rb [options]"
 
-  opts.on("-aACTION", "--action=ACTION", [:launch, :stop, :start, :terminate], "Select action to perform [launch, start, stop, terminate]") do |a|
+  opts.on("-a", "--action=ACTION", [:launch, :stop, :start, :terminate], "Select action to perform [launch, start, stop, terminate]") do |a|
           args.action = a
-	  puts a
   end
   opts.on("-i", "--instance_id=INSTANCE_ID", "ID of the instance to perform an action on") do |i|
 	  args.instance_id = i
-	  puts i
   end
-  opts.on("-v", "--verbose", "") do |b|
+  opts.on("-v", "--verbose", "Run verbosely") do |b|
 	  args.verbose = b
-	  puts b
   end
   opts.on("-h", "--help", "Prints this help") do |h|
     puts opts
@@ -30,3 +28,53 @@ parser = OptionParser.new do |opts|
 end
 
 parser.parse!
+
+creds = YAML.load_file('config.yaml')
+
+ec2 = Aws::EC2::Client.new({
+		region: 		creds['availability-zone'],
+		access_key_id: 		creds['access_key_id'],
+		secret_access_key: 	creds['secret_access_key']
+})
+
+case args.action
+when :launch
+	instance = ec2.run_instances({
+		image_id:		creds['image_id'],
+		min_count: 		1,
+		max_count:		1,
+		key_name:		creds['key_pair'],
+		security_group_ids:	creds['security_group_ids'],
+		instance_type:		creds['instance_type'],
+		placement: {
+			availability_zone: creds['availability-zone']
+		}
+	})
+		id = instance.instances[0].instance_id
+		response = ec2.wait_until(:instance_running, instance_ids:[id])
+		puts id, response.reservations[0].instances[0].public_dns_name
+
+when :stop
+	response = ec2.stop_instances({
+		  instance_ids: [args.instance_id],
+		  force: false,
+	})
+	pp response if args.verbose == true
+
+when :start
+	response = ec2.start_instances({
+		  instance_ids: [args.instance_id], 
+		})
+	response = ec2.wait_until(:instance_running, instance_ids:[args.instance_id])
+	puts response.reservations[0].instances[0].public_dns_name
+	pp response if args.verbose == true
+	
+when :terminate
+	response = ec2.terminate_instances({
+		  instance_ids: [args.instance_id], 
+		})
+	pp response if args.verbose == true
+end
+
+
+
