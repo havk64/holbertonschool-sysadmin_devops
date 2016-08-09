49a77da33955874d05fe3b95ffe394f33a6c1697
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-08-09 02:42:40 -0300

    Credential, region and methods fix, configuration

--- infrastructure_as_code_with_aws_ec2/0-manage_ec2_instances.rb
+++ infrastructure_as_code_with_aws_ec2/0-manage_ec2_instances.rb
@@ -6,13 +6,14 @@ require 'yaml'
 require 'pp'
 
 
-Options = Struct.new(:action,:instance_id, :verbose)
+Options = Struct.new(:action,:instance_id, :verbose, :empty)
 args = Options.new()
+args.empty = ARGV.empty?
 
 parser = OptionParser.new do |opts|
   opts.banner = "Usage: aws_script.rb [options]"
 
-  opts.on("-a", "--action=ACTION", [:launch, :stop, :start, :terminate], "Select action to perform [launch, start, stop, terminate]") do |a|
+  opts.on("-a", "--action=ACTION", [:launch, :stop, :start, :terminate, :list], "Select action to perform [launch, start, stop, terminate, list]") do |a|
           args.action = a
   end
   opts.on("-i", "--instance_id=INSTANCE_ID", "ID of the instance to perform an action on") do |i|
@@ -21,17 +22,21 @@ parser = OptionParser.new do |opts|
   opts.on("-v", "--verbose", "Run verbosely") do |b|
 	  args.verbose = b
   end
-  opts.on("-h", "--help", "Prints this help") do
-    puts opts
-    exit
-  end
 end
 
 parser.parse!
 
+# Prints the help message if no argument is given.
+unless !args.empty
+	puts parser.help
+	exit 1
+end
 creds = YAML.load_file('config.yaml')
-
-ec2 = Aws::EC2::Resource.new(region: 'us-west-2')
+Aws.config.update({
+	region: creds['availability-zone'],
+	credentials: Aws::Credentials.new(creds['access_key_id'], creds['secret_access_key'])
+})
+ec2 = Aws::EC2::Resource.new()
 
 case args.action
 when :launch
@@ -42,34 +47,49 @@ when :launch
 		key_name:		creds['key_pair'],
 		security_group_ids:	creds['security_group_ids'],
 		instance_type:		creds['instance_type'],
-		placement: {
-			availability_zone: creds['availability-zone']
-		}
 	})
 		id = instance[0].id
 		response = ec2.client.wait_until(:instance_running, instance_ids:[id])
 		puts id + "," + response.reservations[0].instances[0].public_dns_name
-		puts "The whole response Object: "
-		pp response.to_h if args.verbose == true
+		if args.verbose == true
+			puts "The whole response Object: "
+			response.to_h
+		end
 
 when :stop
-	response = ec2.stop_instances({
+	response = ec2.client.stop_instances({
 		  instance_ids: [args.instance_id],
 		  force: false,
 	})
 	pp response.to_h if args.verbose == true
 
 when :start
-	response = ec2.start_instances({
+	response = ec2.client.start_instances({
 		  instance_ids: [args.instance_id], 
 		})
-	response = ec2.wait_until(:instance_running, instance_ids:[args.instance_id])
+	response = ec2.client.wait_until(:instance_running, instance_ids:[args.instance_id])
 	puts response.reservations[0].instances[0].public_dns_name
 	pp response.to_h if args.verbose == true
 	
 when :terminate
-	response = ec2.terminate_instances({
+	response = ec2.client.terminate_instances({
 		  instance_ids: [args.instance_id], 
 		})
 	pp response.to_h if args.verbose == true
+
+when :list
+	resp = ec2.instances.inject({}) { |m, i| m[i.id] = i.state; m }
+	puts resp.to_yaml
+	ec2.instances.each do |i|
+		puts i.public_dns_name
+		puts i
+		#puts "Instance id: " + i.methods
+	end
+	ec2.instances[0].each do |i|
+		puts i.inspect
+	end
+
+	puts "===================================="
+	puts ec2.inspect
 end
+
