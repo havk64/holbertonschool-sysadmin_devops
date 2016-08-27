bce9e08c5594f0b8366f8705763ecb3952bda381
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-08-26 23:01:42 -0300

    Indentation

--- build_your_aws_s3_script/0-basic_aws_s3_script.rb
+++ build_your_aws_s3_script/0-basic_aws_s3_script.rb
@@ -11,24 +11,24 @@ args = Options.new()
 args.empty = ARGV.empty?
 
 parser = OptionParser.new do |opts|
-  opts.banner = "Usage: s3_script.rb [options]"
-  
-  opts.on("-v", "--verbose", "Run verbosely") do |b|
-	  args.verbose = b
-  end
-  opts.on("-b", "--bucketname=BUCKET_NAME", "Name of the bucket to perform the action on") do |n|
-	  args.name = n
-  end
-  opts.on("-f", "--filepath=FILE_PATH", "Path to the file to upload") do |f|
-	  args.path = f
-  end
-  opts.on("-a", "--action=ACTION", [:list, :upload, :delete, :download], "Select action to perform [list, upload, delete, download]") do |a|
-	  args.action = a
-  end
-  opts.on("-h", "--help", "Returns the help menu") do
-	  puts opts
-	  exit
-  end
+	opts.banner = "Usage: s3_script.rb [options]"
+
+	opts.on("-v", "--verbose", "Run verbosely") do |b|
+		args.verbose = b
+	end
+	opts.on("-b", "--bucketname=BUCKET_NAME", "Name of the bucket to perform the action on") do |n|
+		args.name = n
+	end
+	opts.on("-f", "--filepath=FILE_PATH", "Path to the file to upload") do |f|
+		args.path = f
+	end
+	opts.on("-a", "--action=ACTION", [:list, :upload, :delete, :download], "Select action to perform [list, upload, delete, download]") do |a|
+		args.action = a
+	end
+	opts.on("-h", "--help", "Returns the help menu") do
+		puts opts
+		exit
+	end
 end
 
 parser.parse!
