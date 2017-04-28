c17581567b608391809a73dd95cd7683d1c8f00b
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-04-28 01:07:39 -0300

    Update comments, add function to check kind of file to define output format

--- 0x05-processes_and_signals/task10_unit_tests/filemonitor.sh
+++ 0x05-processes_and_signals/task10_unit_tests/filemonitor.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-# This scripts makes use of inotifywait that is part of inotify-tools package
+# This scripts start monitoring files or directories for any changes
+# It makes use of inotifywait that is part of inotify-tools package.
+# The output goes to stdout
 
 EXEC='/usr/bin/inotifywait'
 
@@ -9,13 +11,26 @@ usage()
 	exit 1
 }
 
+checkDir()
+{
+	# Case the file to be monitored doesn't exist, waits for its creation
+	while [[ ! -e $1 ]]; do sleep 0.5; done
+	# Default format for directories
+	local format='%T: %f %:e'
+	# Case is a file instead change the output format
+	[[ -f "$1" ]] && format='%T: %w %:e'
+	echo "$format"
+}
+
+
+# Case the number of arguments is less then one or greater then 2, prints usage
 [[ $# -lt 1 ]] || [[ $# -gt 2 ]] && usage
 
 initMonitor() {
-	echo "Waiting for file $(pwd)/$DIR"
-	while [[ ! -e $DIR ]]; do sleep 0.5; done
-	"$EXEC" -m --timefmt '%r' --format '%T: %f %:e' "$DIR" |
-	while read -r change; do echo "$change"; done
+	echo "Waiting for file $(pwd)/$1"
+	format=$(checkDir "$1")
+	"$EXEC" -m --timefmt '%r' --format "$format" "$1" |
+	while read -r change; do echo "$change"; done | grep -v 'utmp'
 }
 
 stopMonitor() {
@@ -24,6 +39,7 @@ stopMonitor() {
 
 case "$1" in
 	start)
+		# Case second positional arg isn't given defaults to current dir
 		DIR=${2-.}
 		initMonitor "$DIR"
 		;;
@@ -33,3 +49,4 @@ case "$1" in
 	*)
 		usage
 esac
+exit
